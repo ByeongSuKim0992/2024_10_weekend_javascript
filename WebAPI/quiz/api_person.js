15b1179954b5bea643bc798bc0189c91605e61ba
Kim Byeongsu <byeongsu0707@gmail.com>
2024-12-08 21:44:38 +0900

    Add files via upload

--- WebAPI/quiz/api_person.js
+++ WebAPI/quiz/api_person.js
@@ -1,9 +1,3 @@
-// const button = document.querySelector("#button");
-
-// fetch("https://fakerapi.it/api/v2/persons?_quantity=10")
-//   .then((response) => response.json())
-//   .then((v) => console.log(v.data));
-
 const button = document.querySelector("#button");
 
 button.addEventListener("click", () => {
